a963d41b2f982d8993c606b5a39b9463e6812e07
saispandana7935 <saispandana7935@gmail.com>
2020-12-25 22:23:50 +0530

    REFACTOR:calculate wages in case statement

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -3,16 +3,13 @@ isFullTime=1;
 isPartTime=2;
 randomCheck=$((RANDOM % 3));
 wagePerHr=20;
-if [ $isFullTime -eq $randomCheck ];
-then
-        workingHrs=8;
-elif [ $isPartTime -eq $randomCheck ];
-then
-        workingHrs=4;
-else
-        workingHrs=0;
+case $randomCheck in
+        $isFullTime)
+        workingHrs=8 ;;
+        $isPartTime )
+        workingHrs=4 ;;
+        *)
+        workingHrs=0 ;;
 
-fi
+esac
 Salary=$(( $wagePerHr*$workingHrs));
-
-echo "salary received by employee is" $Salary
