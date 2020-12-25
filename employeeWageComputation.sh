fa63cd9d3a3f32c4ee06354845c396319344479d
saispandana7935 <saispandana7935@gmail.com>
2020-12-25 21:33:37 +0530

    REFACTOR:calculate daily wage

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,9 +1,14 @@
+#! /bin/bash -x
 isPresent=1
 isAbsent=0
+wagePerHr=20
+fullDay=8
 check=$((RANDOM%2))
-if [ check -eq isPresent ]
+if [ $check -eq $isPresent ]
 then
   echo "Employee is Present "
+   Salary=$(( $wagePerHr*$fullDay ))
 else
   echo " Employee is Absent "
+   Salary=0
  fi
