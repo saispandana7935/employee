edde8191d5edb0f841873494d6ab7b0ab58b65ba
saispandana7935 <saispandana7935@gmail.com>
2020-12-26 08:20:14 +0530

    REFACTOR:get working hours using Functions.

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,29 +1,32 @@
 #! /bin/bash -x
 isFullTime=1;
-workingHrs=0
 isPartTime=2;
 wagePerHr=20;
 workingDays=0;
 totalWorkHrs=100;
 totalWorkDays=20;
-totalSalary=0;
-while [[ $workingHrs -le $totalWorkHrs && $workingDays -lt $totalWorkDays ]]
+totalWorkingHrs=0
+
+function getWorkingHrs(){
+	empCheck=$1
+	case $empCheck in
+        	$isFullTime)
+        		workingHrs=8 ;;
+        	$isPartTime )
+        		workingHrs=4 ;;
+        	*)
+        		workingHrs=0 ;;
+
+	esac
+		echo $workingHrs
+}
+
+while [[ $totalWorkingHrs -le $totalWorkHrs && $workingDays -le $totalWorkDays ]]
 
 do
 	workingDays=$((workingDays+1))
-	empCheck=$((RANDOM % 3));
-
-	case $empCheck in
-        	$isFullTime)
-        	workingHrs=8 ;;
-        	$isPartTime )
-        	workingHrs=4 ;;
-        	*)
-        	workingHrs=0 ;;
-
-	esac
-	workingHrs+=$(($workingHrs))
-
+	workingHrs="$(getWorkingHrs $((RANDOM%3)) )"
+	totalWorkingHrs=$((totalWorkingHrs+workingHrs))
 
 done
-	Salary=$(( $wagePerHr*$workingHrs ))
+	Salary=$(( $wagePerHr*$totalWorkingHrs ))
